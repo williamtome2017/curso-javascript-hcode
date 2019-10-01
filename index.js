18deab743f0b5d58d951abbd901d8afa607fea1e
William Tomé <william.tomenh@gmail.com>
2019-09-30 21:19:03 -0300

    First commit.

---
+++ index.js
@@ -0,0 +1,20 @@
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+var user = {};
+
+document.getElementById('form-user-create').addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    fields.forEach(function (field, index) {
+
+        if (field.name == 'gender' && field.checked) {
+            user[field.name] = field.value
+
+        } else {
+            user[field.name] = field.value
+        }
+    });  
+    
+    console.log(user);    
+
+})
