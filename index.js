057d6bc8871f318b866094cfad2dc0e493c57ba7
William Tomé <william.tomenh@gmail.com>
2019-09-30 22:59:54 -0300

    Instanciated the object User on the function dataUser and show the data on screen.

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
 function addLine(dataUser) {
-    
-    var tr = document.createElement('tr');
-    tr.innerHTML = `
+
+    document.getElementById('table-users').innerHTML = `
         <tr>
             <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
@@ -16,9 +15,7 @@ function addLine(dataUser) {
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
         </tr>
-    `;
-
-    document.getElementById('table-users').appendChild(tr);
+    `;    
 
 }
 
@@ -27,14 +24,27 @@ document.getElementById('form-user-create').addEventListener("submit", function(
     
     fields.forEach(function (field, index) {
 
-        if (field.name == 'gender' && field.checked) {
-            user[field.name] = field.value
+        if (field.name == 'gender') {
+            if (field.checked) {                
+                user[field.name] = field.value
+            }
 
         } else {
             user[field.name] = field.value
         }
     });  
+
+    var objectUser = new User(
+        user.name, 
+        user.gender, 
+        user.birth, 
+        user.country, 
+        user.email, 
+        user.password, 
+        user.photo, 
+        user.admin
+    );
     
-    addLine(user);    
+    addLine(objectUser);    
 
 })
