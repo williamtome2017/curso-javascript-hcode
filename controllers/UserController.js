cae38b3520fe2fdc41a36b53a03380a14214be08
William Tomé <william.tomenh@gmail.com>
2019-10-02 14:18:25 -0300

    Adjust of the view on the project - ternary operator.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -86,7 +86,11 @@ class UserController {
                     user[field.name] = field.value
                 }
 
+            } else if(field.name == 'admin'){
+
+                user[field.name] = field.checked;
             } else {
+
                 user[field.name] = field.value
             }
         });
@@ -106,18 +110,21 @@ class UserController {
 
     addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
-            <tr>
-                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${(dataUser.admin == 'on') ? 'Sim' : 'Não'}</td>
-                <td>${dataUser.birth}</td>
-                <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr>`;
+        let tr = document.createElement('tr')
+
+
+        tr.innerHTML = `
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>`;
+
+        this.tableEl.appendChild(tr)
     }
 
 }
