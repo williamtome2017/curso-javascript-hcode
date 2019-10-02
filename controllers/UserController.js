d081004a5171fdf3d17e6a423167c9bdd973b53d
William Tomé <william.tomenh@gmail.com>
2019-10-02 13:52:33 -0300

    Working with Promise.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -16,42 +16,58 @@ class UserController {
             event.preventDefault();
 
             let values = this.getValues();
+
+            this.getPhoto().then(
+                (content) => {
+
+                    values.photo = content;
+
+                    this.addLine(values);
+
+                },
+                (e) => {
+
+                    console.error(e);
+                }
+            );
             
-            this.getPhoto((content)=>{
-
-                values.photo = content;
-
-                this.addLine(values);
-
-            });
-
         })
 
     }
 
-    getPhoto(callback){
-        
-        let fileReader = new FileReader();
-        
-        let elements = [...this.formEl.elements].filter(item=>{
+    getPhoto(){
+
+        return new Promise((resolve, reject)=>{
             
-            if(item.name === 'photo') {
-
-                return item;
-
+            let fileReader = new FileReader();
+            
+            let elements = [...this.formEl.elements].filter(item=>{
+                
+                if(item.name === 'photo') {
+    
+                    return item;
+    
+                }
+    
+            })
+            
+            let file = elements[0].files[0];
+    
+            fileReader.onload = () => {
+    
+                resolve(fileReader.result)
+    
             }
 
+            fileReader.onerror = (e)=>{
+                
+                reject(e)
+
+            }
+    
+            fileReader.readAsDataURL(file);
         })
         
-        let file = elements[0].files[0];
-
-        fileReader.onload = () => {
-
-            callback(fileReader.result)
-
-        }
-
-        fileReader.readAsDataURL(file);
     }
 
     // getter
