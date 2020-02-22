cf3114d9e264be018b1637698b90309a069954a6
William W. Tome <william.tomenh@gmail.com>
2020-02-22 18:48:44 -0300

    Using dataset to change count of the user registered.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -131,7 +131,7 @@ class UserController {
     addLine(dataUser) {
 
         let tr = document.createElement('tr')
-
+        tr.dataset.user = JSON.stringify(dataUser);
 
         tr.innerHTML = `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
@@ -145,6 +145,22 @@ class UserController {
             </td>`;
 
         this.tableEl.appendChild(tr)
+
+        this.updateCount();
+    }
+
+    updateCount() {
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+        [...this.tableEl.children].forEach(tr =>{
+            numberUsers++;
+            let user = JSON.parse(tr.dataset.user);
+            if(user._admin) numberAdmin++;
+        })
+
+        document.querySelector('#number-users').innerHTML = numberUsers;
+        document.querySelector('#number-users-admin').innerHTML = numberAdmin;
     }
 
 }
