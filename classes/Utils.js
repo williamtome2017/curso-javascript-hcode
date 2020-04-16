c8b8b5ed858196cd103db2c9ca9be08cd7d17bd5
William Tomé <william.tomenh@gmail.com>
2020-04-15 21:28:53 -0300

    Adjust date format.

--- classes/Utils.js
+++ classes/Utils.js
@@ -2,7 +2,7 @@ class Utils {
 
     static dateFormat(date){
 
-        return date.getDate()+'/'+(date.getMonth()+1)+date.getFullYear()+' '+date.getHours()+':'+date.getMinutes();
+        return date.getDate()+'/'+(date.getMonth()+1)+"/"+date.getFullYear()+' '+date.getHours()+':'+date.getMinutes();
         
     }
 
