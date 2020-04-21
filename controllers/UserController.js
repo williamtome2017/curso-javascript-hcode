4eb19eb47f87d3a7ef51b9e0207ef75d82246427
William Tomé <william.tomenh@gmail.com>
2020-04-21 17:41:00 -0300

    Refactoring the create and edit tr element.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -47,20 +47,11 @@ class UserController {
                         result._photo = content;
                     }
 
-                    tr.dataset.user = JSON.stringify(result);
+                    let user = new User();
 
-                    tr.innerHTML = `
-                        <td><img src="${result._photo}" alt="User Image" class="img-circle img-sm"></td>
-                        <td>${result._name}</td>
-                        <td>${result._email}</td>
-                        <td>${(result._admin) ? 'Sim' : 'Não'}</td>
-                        <td>${Utils.dateFormat(result._register)}</td>
-                        <td>
-                            <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                        </td>`;
+                    user.loadFromJSON(result);
 
-                    this.addEventsTr(tr);
+                    this.getTr(user, tr);
 
                     this.updateCount();
 
@@ -240,7 +231,16 @@ class UserController {
 
     addLine(dataUser) {
 
-        let tr = document.createElement('tr')
+        let tr = this.getTr(dataUser);
+
+        this.tableEl.appendChild(tr);
+
+        this.updateCount();
+    }
+
+    getTr(dataUser, tr = null){
+
+        if (tr === null) tr = document.createElement('tr');
 
         tr.dataset.user = JSON.stringify(dataUser);
 
@@ -257,9 +257,8 @@ class UserController {
 
         this.addEventsTr(tr);
 
-        this.tableEl.appendChild(tr)
+        return tr;
 
-        this.updateCount();
     }
 
     showPanelCreate(){
