513e6304f75f23c0e314c6265e99ca3e594b29f1
William Tomé <william.tomenh@gmail.com>
2020-04-21 16:58:41 -0300

    Save data in session storage.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,6 +8,7 @@ class UserController {
 
         this.onSubmit();
         this.onEdit();
+        this.selectAll();
 
     }
 
@@ -98,6 +99,8 @@ class UserController {
                 (content) => {
 
                     values.photo = content;
+
+                    this.insert(values);
 
                     this.addLine(values);
 
@@ -199,9 +202,46 @@ class UserController {
 
     }
 
+    getUsersStorage(){
+
+        let users = [];
+
+        if (sessionStorage.getItem('users')) {
+            users = JSON.parse(sessionStorage.getItem('users'));
+        }
+
+        return users;
+
+    }
+
+    selectAll(){
+
+        let users = this.getUsersStorage();
+
+        users.forEach(dataUser => {
+
+            let user = new User();
+
+            user.loadFromJSON(dataUser);
+
+            this.addLine(user);
+        })
+
+    }
+
+    insert(data){
+        
+        let users = this.getUsersStorage();
+
+        users.push(data)
+
+        sessionStorage.setItem('users', JSON.stringify(users))
+    }
+
     addLine(dataUser) {
 
         let tr = document.createElement('tr')
+
         tr.dataset.user = JSON.stringify(dataUser);
 
         tr.innerHTML = `
