1c278a1ddd57974b9bbc5f4583862080fa77e623
William Tomé <william.tomenh@gmail.com>
2020-04-22 23:31:40 -0300

    create getters and setters, constructor and initialize

--- calculadora/scripts/controller/CalcController.js
+++ calculadora/scripts/controller/CalcController.js
@@ -1,5 +1,37 @@
 class CalcController {
 
-    
+	constructor() {
+
+		this._displayCalc = "0";
+		this._currentDate;
+		this.initialize();
+
+	}
+
+	initialize(){
+		let displayCalcEl = document.querySelector('#display');
+		let dateEl = document.querySelector('#data');
+		let timeEl = document.querySelector('#hora');
+
+		displayCalcEl.innerHTML = '4567';
+		dateEl.innerHTML = '25/12/2020';
+		timeEl.innerHTML = '00:01';
+	}
+
+	get displayCalc() {
+		return this._displayCalc;
+	}
+
+	get dataAtual() {
+		return this._currentDate;
+	}
+
+	set displayCalc(value) {
+		this._displayCalc = value;
+	}
+
+	set dataAtual(value) {
+		this._currentDate = value;
+	}
 
 }
