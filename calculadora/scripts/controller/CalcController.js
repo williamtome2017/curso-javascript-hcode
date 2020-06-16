11c9fc468fa7d1c33972460b9655150e254c93ee
William Tomé <william.tomenh@gmail.com>
2020-06-15 23:02:47 -0300

    Implementado a exibição das datas e hora atual.

--- calculadora/scripts/controller/CalcController.js
+++ calculadora/scripts/controller/CalcController.js
@@ -1,37 +1,65 @@
-class CalcController {
+class calcController {
 
 	constructor() {
 
-		this._displayCalc = "0";
+		this._locale = 'pt-BR';
+		this._displayCalcEl = document.querySelector('#display');
+		this._dateEl = document.querySelector('#data');
+		this._timeEl = document.querySelector('#hora');
 		this._currentDate;
 		this.initialize();
 
 	}
 
 	initialize() {
-		let displayCalcEl = document.querySelector('#display');
-		let dateEl = document.querySelector('#data');
-		let timeEl = document.querySelector('#hora');
 
-		displayCalcEl.innerHTML = '4567';
-		dateEl.innerHTML = '25/12/2020';
-		timeEl.innerHTML = '00:01';
+		this.setDisplayDateTime();
+
+		setInterval(() => {
+			this.setDisplayDateTime();
+		}, 1000);
+
+	}
+
+	setDisplayDateTime() {
+		this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+			day: "2-digit",
+			month: "long",
+			year: "numeric"
+		});
+		this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+	}
+
+	get displayTime() {
+		return this._timeEl.innerHTML;
+	}
+
+	set displayTime(value) {
+		this._timeEl.innerHTML = value;
+	}
+
+	get displayDate() {
+		return this._timeEl.innerHTML;
+	}
+
+	set displayDate(value) {
+		this._dateEl.innerHTML = value;
 	}
 
 	get displayCalc() {
-		return this._displayCalc;
-	}
-
-	get dataAtual() {
-		return this._currentDate;
+		return this._displayCalcEl.innerHTML;
 	}
 
 	set displayCalc(value) {
-		this._displayCalc = value;
+		this._displayCalcEl.innerHTML = value;
 	}
 
-	set dataAtual(value) {
-		this._currentDate = value;
+	get currentDate() {
+		return new Date();
+	}
+
+	set currentDate(value) {
+		this._dateEl.innerHTML = value;
 	}
 
 }
