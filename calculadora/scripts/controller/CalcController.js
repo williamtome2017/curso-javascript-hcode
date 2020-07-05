f565c499e8f8c1010ebf5b112bbde0fac7e79068
William Tomé <william.tomenh@gmail.com>
2020-07-04 21:41:30 -0300

    Adicionado try/catch no getResult

--- calculadora/scripts/controller/CalcController.js
+++ calculadora/scripts/controller/CalcController.js
@@ -152,7 +152,13 @@ class calcController {
 	}
 
 	getResult() {
-		return eval(this._operation.join(""));
+		try {
+			return eval(this._operation.join(""));
+		} catch (e) {
+			setTimeout(() => {
+				this.setError();
+			}, 1);
+		}
 	}
 
 	calc() {
