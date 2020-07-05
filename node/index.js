8ff192640ce9c7fa09dd9fff22b0dc5130997a95
William Tomé <william.tomenh@gmail.com>
2020-07-05 18:29:22 -0300

    Iniciando com NodeJS

---
+++ node/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+    console.log('URL: ', req.url);
+    console.log('METHOD: ', req.method);
+    res.end('OK')
+})
+
+server.listen(3000, '127.0.0.1', ()=>{
+    console.log('Servidor rodando...');    
+})
